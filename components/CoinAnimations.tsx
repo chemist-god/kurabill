15ba12641a45347cf6eab6489dd5f8ca61e8df41
chemist-god <godtenkorang72@gmail.com>
2025-02-27 23:25:21 +0000

    Refactor hero-section and add new components for MedicationBubbles, CountryProfiles, CoinAnimations,  with enhanced functionality and SVG icons

---
+++ components/CoinAnimations.tsx
@@ -0,0 +1,55 @@
+// components/CoinAnimations.tsx
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const CoinAnimations = () => {
+  return (
+    <>
+      {/* First Coin */}
+      <motion.div
+        className="absolute z-10 top-[7%] right-[12%]"
+        animate={{
+          y: [-10, 0, -10],
+          rotate: [0, 360]
+        }}
+        transition={{
+          duration: 4,
+          repeat: Infinity,
+          ease: "easeInOut"
+        }}
+      >
+        <Image
+          src="/assets/images/coins.svg"
+          alt="Stable coin"
+          width={32}
+          height={20}
+          className="w-full h-full object-contain"
+        />
+      </motion.div>
+      {/* Second Coin */}
+      <motion.div
+        className="absolute z-10 bottom-[40%] left-[8%]"
+        animate={{
+          y: [-20, 0, -20],
+          rotate: [0, 360]
+        }}
+        transition={{
+          duration: 4,
+          repeat: Infinity,
+          ease: "easeInOut",
+          delay: 1
+        }}
+      >
+        <Image
+          src="/assets/images/coin.svg"
+          alt="Stable coin"
+          width={78}
+          height={78}
+          className="w-full h-full object-contain"
+        />
+      </motion.div>
+    </>
+  );
+};
+
+export default CoinAnimations;

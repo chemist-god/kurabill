ddd1a954ee8fad3bc9eb8def913c3d9b3fb99c1f
chemist-god <godtenkorang72@gmail.com>
2025-03-31 00:05:53 +0000

    Update footer pharmacist-specific routes

--- components/footer-component/footer.tsx
+++ components/footer-component/footer.tsx
@@ -53,9 +53,9 @@ export function Footer() {
             <h3 className="font-semibold text-lg">For Pharmacists</h3>
             <ul className="space-y-2">
               <li><Link href="/pharmacists/pricing" className="hover:underline">Pricing</Link></li>
-              <li><Link href="/testimonials" className="hover:underline">Testimonials</Link></li>
-              <li><Link href="/faqs" className="hover:underline">FAQs</Link></li>
-              <li><Link href="/about" className="hover:underline">About Us</Link></li>
+              <li><Link href="/pharmacists/testimonials" className="hover:underline">Testimonials</Link></li>
+              <li><Link href="/pharmacists/faqs" className="hover:underline">FAQs</Link></li>
+              <li><Link href="/pharmacists/about" className="hover:underline">About Us</Link></li>
             </ul>
           </div>
 
